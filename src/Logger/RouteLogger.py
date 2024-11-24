4fcfe40557b282efe28d8c5b79ed8190dfcb245e
RomanPorshnev <unknwnbeats111@gmail.com>
2024-11-24 03:33:17 +0300

    fix magic numbers

--- src/Logger/RouteLogger.py
+++ src/Logger/RouteLogger.py
@@ -6,13 +6,16 @@ class RouteLogger(Logger):
         super().__init__("[RouteInfo]")
 
     def print_routes_data_info(self, message, target_nodes_data):
+        number_of_displayed_target_nodes = 5
+        first_column_size = 14
+        second_column_size = 46
         print(f"{self._logger_type} {message.value}")
         table = PrettyTable()
         table.field_names = ["target node id", "number of paths, that ends in this target node"]
         if len(target_nodes_data) > 11:
-            for target_node_data in target_nodes_data[:5]:
+            for target_node_data in target_nodes_data[:number_of_displayed_target_nodes]:
                 table.add_row([target_node_data.node_id, len(target_node_data.start_nodes_ids)])
-            table.add_row(["." * 14, "." * 46])
+            table.add_row(["." * first_column_size, "." * second_column_size])
             for target_node_data in target_nodes_data[-5:]:
                 table.add_row([target_node_data.node_id, len(target_node_data.start_nodes_ids)])
         else:
