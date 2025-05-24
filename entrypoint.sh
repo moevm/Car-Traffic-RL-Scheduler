f3f8abfe540d2ed7911a9316502e658427747d92
RomanPorshnev <unknwnbeats111@gmail.com>
2025-05-24 22:58:18 +0300

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ export SUMO_HOME="/usr/local/share/sumo"
 
 source sumovenv/bin/activate
 
-tensorboard --logdir /app/ppo_traffic_lights_tensorboard --host=0.0.0.0 &
+tensorboard --logdir /app/metrcis_logs --host=0.0.0.0 &
 
 exec python3 main.py \
   -s configs/static_tls/cycle_time_10000/rand_40.sumocfg \
